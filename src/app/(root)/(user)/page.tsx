8df0ac28c20208ced0c1d6b5047771ac3a8c7074
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-06-26 23:48:59 +0100

    home page animation

--- src/app/(root)/(user)/page.tsx
+++ src/app/(root)/(user)/page.tsx
@@ -7,6 +7,9 @@ import Link from "next/link";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 const FeaturesList = [
   "Automatically detect subjects on photos",
   "Neat, clear & smooth cutout edges",
@@ -15,11 +18,42 @@ const FeaturesList = [
 ];
 
 export default function Home() {
+  useGSAP(() => {
+    gsap.to("#features_title", {
+      opacity: 1,
+      duration: 1,
+      y: 0,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: ".features_title",
+        start: "top 0%", // Adjust as needed
+        end: "bottom 0%",
+        toggleActions: "play none none reverse",
+      },
+    });
+    gsap.to(".feature_item", {
+      opacity: 1,
+      x: 0,
+      duration: 1,
+      ease: "power2.out",
+      stagger: 0.3,
+      scrollTrigger: {
+        trigger: ".feature_item",
+        start: "top 80%", // Adjust as needed
+        end: "bottom 60%",
+        toggleActions: "play none none reverse",
+      },
+    });
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16">
       <Hero />
       <div className="mt-48 gap-20 flex items-center justify-center flex-col">
-        <h1 className="text-2xl md:text-4xl font-bold">
+        <h1
+          id="features_title"
+          className="opacity-0 translate-y-20 text-2xl md:text-4xl font-bold"
+        >
           What BGR.AI Can Do For You...
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 w-full md:w-[70%] gap-8">
@@ -36,7 +70,10 @@ export default function Home() {
             </p>
             <ul>
               {FeaturesList.map((item) => (
-                <li key={item} className="flex flex-row items-center gap-2 ">
+                <li
+                  key={item}
+                  className="feature_item flex translate-x-48 opacity-0 flex-row items-center gap-2 "
+                >
                   <div className="p-1 w-fit h-fit rounded-full bg-primary"></div>{" "}
                   {item}
                 </li>

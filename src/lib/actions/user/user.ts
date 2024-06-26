a40f36f576171b5d864942e276b93c2142e88e6a
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-06-26 19:28:17 +0100

    find user

--- src/lib/actions/user/user.ts
+++ src/lib/actions/user/user.ts
@@ -3,6 +3,21 @@ import { RegisterSchema } from "@/lib/validators/user";
 import { revalidatePath } from "next/cache";
 import { notFound } from "next/navigation";
 
+export async function getUserById(id: string) {
+  const user = await db.user.findUnique({ where: { id } });
+  return user;
+}
+
+export async function getUserByClerkId(clerkId: string) {
+  const user = await db.user.findUnique({ where: { clerkId } });
+  return user;
+}
+
+export async function getUserByEmail(email: string) {
+  const user = await db.user.findUnique({ where: { email: email } });
+  return user;
+}
+
 export async function createUser(prevState: unknown, formData: FormData) {
   const result = RegisterSchema.safeParse(
     Object.fromEntries(formData.entries())

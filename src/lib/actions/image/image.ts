a715a9a6bc9ffcfefe98c03ac0eafe7a3b697d34
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-06-26 19:31:34 +0100

    image check credits init

--- src/lib/actions/image/image.ts
+++ src/lib/actions/image/image.ts
@@ -1,2 +1,11 @@
 import db from "@/db/db";
+import { getUserById } from "../user/user";
+import { notFound } from "next/navigation";
 
+export async function checkCreditsPermission(id: string) {
+  const user = getUserById(id);
+  if (user == null) return notFound();
+
+  //   to do : check if credits are still available
+  
+}

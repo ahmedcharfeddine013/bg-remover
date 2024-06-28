c2ae274343b8e973bb0f7b332289911bfa0a4f83
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-06-28 15:11:13 +0100

    webhook testing success

--- src/app/api/clerk/route.ts
+++
@@ -1,7 +0,0 @@
-import { WebhookEvent } from "@clerk/nextjs/server";
-
-// export async function POST(request: Request) {}
-
-export async function Get() {
-  return Response.json({ message: "Hello world!" });
-}
